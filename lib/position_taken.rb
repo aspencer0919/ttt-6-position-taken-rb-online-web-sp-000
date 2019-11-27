23c30587afa0a29bebc8f6a60d390c57a802902a
Adam Spencer <aspencer0919@gmail.com>
2019-11-27 14:54:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,7 +5,7 @@ index = 0
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     return true
-  elsif board[index] == " " || board[index] == ""
+  elsif board[index] == " " || board[index] == "" || board[index] == nil
     return false
   else
     return true
